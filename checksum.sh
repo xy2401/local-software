70e7d7f563d6d5215d82e958bb837ac0cc312feb
xy2401 <xy2401@163.com>
2021-02-22 20:08:34 +0800

    处理文件名空格

--- checksum.sh
+++ checksum.sh
@@ -20,15 +20,15 @@ find ./dl -type f -print0 | while IFS= read -r -d $'\0' file;
   do 
   if [[ "$checksum" != *"$file"* ]]; then
     echo "new file $file" ; 
-    md5=$(    md5sum    $file| cut -d ' ' -f 1) 
-    sha1=$(   sha1sum   $file| cut -d ' ' -f 1) 
-    sha256=$( sha256sum $file| cut -d ' ' -f 1) 
+    md5=$(    md5sum    "$file"| cut -d ' ' -f 1) 
+    sha1=$(   sha1sum   "$file"| cut -d ' ' -f 1) 
+    sha256=$( sha256sum "$file"| cut -d ' ' -f 1) 
     
     ##懒得计算该如何显示KB/MB/GB 直接 -h
     size=`du -h "$file" | cut -f1`
     mdate=`date -r "$file"  "+%Y-%m-%d"`
 
-    echo "| `printf "%-32s" $mdate` | `printf "%-40s" $size` | `printf "%-64s" $file` |"  >> ./docs/checksum.md;
+    echo "| `printf "%-32s" $mdate` | `printf "%-40s" $size` | `printf "%-64s" "$file"` |"  >> ./docs/checksum.md;
     echo "| $md5 | $sha1 | $sha256 |" >> ./docs/checksum.md;
     
   fi
